95c9695a9c90f5a41bd9d2124dd20f189ac3898a
vaibhavyadnik <vaibhavyadnik187@gmail.com>
2020-04-26 18:37:49 +0530

    Print print times

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+// Simple Java Programs to Print Hello World
+public class HelloWorld {
+
+	public static void main(String[] args)
+	{
+		int i;
+		for(i = 0; i <= 5; i++)
+		{
+			System.out.println("\n Hello World ");
+		}	
+	}
+}
